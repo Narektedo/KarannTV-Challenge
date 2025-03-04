43028e4bf48a001f1c7224a6488c966aaeaaafe6
WaloPVG <victor.barriere@outlook.fr>
2025-03-04 19:10:44 +0100

    XD 21

--- src/pages/pagetest.jsx
+++ src/pages/pagetest.jsx
@@ -6,7 +6,7 @@ function App() {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch("http://localhost:8080/data") // Appel au backend
+        fetch("https://walopvgapi-9c205847a91e.herokuapp.com/info/PVG/EUW") // Appel au backend
             .then(response => {
                 if (!response.ok) {
                     throw new Error("Erreur lors de la récupération des données !");

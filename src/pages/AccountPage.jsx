b85072d1e762724fe80518d38f0b1a7306e0f7de
WaloPVG <victor.barriere@outlook.fr>
2025-03-07 22:26:27 +0100

    jsuis juste con oklm

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -28,6 +28,11 @@ function Profile() {
             <h1 className="container">Données du backend</h1>
             {error && <p style={{ color: "red" }}>{error.message}</p>}
             <pre>{data ? JSON.stringify(data, null, 2) : "Chargement..."}</pre>
+            <img
+                        src={`https://ddragon.leagueoflegends.com/cdn/15.4.1/img/profileicon/6044.png`}
+                        alt="Icône du joueur"
+                        style={{ width: 100, height: 100, borderRadius: "50%" }}
+                    />
         </div>
     );
 }

2727ff6f6ea6f4e08eca426d497c4b3403dac7d7
WaloPVG <victor.barriere@outlook.fr>
2025-03-08 00:30:20 +0100

    ergerg

--- src/pages/pagetestwollows.jsx
+++ src/pages/pagetestwollows.jsx
@@ -27,13 +27,11 @@ function Profile() {
             <h1 className="container">Données du backend</h1>
             {error && <p style={{ color: "red" }}>{error.message}</p>}
 
-            {data && data.summonerInfo && data.summonerInfo.iconId && (
                 <img
                     src={`https://ddragon.leagueoflegends.com/cdn/15.4.1/img/profileicon/${data.summonerInfo.iconId}.png`}
                     alt="Icône du joueur"
                     style={{ width: 100, height: 100, borderRadius: "50%" }}
                 />
-            )}
 
             <pre>{data ? JSON.stringify(data, null, 2) : "Chargement..."}</pre>
         </div>

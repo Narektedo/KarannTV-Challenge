cc6187e72724f8e14bd49f8b4d550bd2c3904c9d
WaloPVG <victor.barriere@outlook.fr>
2025-03-08 00:22:21 +0100

    summonerInfo

--- src/pages/pagetestwollows.jsx
+++ src/pages/pagetestwollows.jsx
@@ -27,10 +27,9 @@ function Profile() {
             <h1 className="container">Données du backend</h1>
             {error && <p style={{ color: "red" }}>{error.message}</p>}
 
-            {/* Affichage de l'icône du joueur en fonction du iconId */}
-            {data && data.iconId && (
-                <img className="container"
-                    src={`https://ddragon.leagueoflegends.com/cdn/15.4.1/img/profileicon/${data.iconId}.png`}
+            {data && data.summonerInfo && data.summonerInfo.iconId && (
+                <img
+                    src={`https://ddragon.leagueoflegends.com/cdn/15.4.1/img/profileicon/${data.summonerInfo.iconId}.png`}
                     alt="Icône du joueur"
                     style={{ width: 100, height: 100, borderRadius: "50%" }}
                 />

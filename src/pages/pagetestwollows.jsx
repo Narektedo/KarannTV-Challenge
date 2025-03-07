41b636c23eac9f11e4bcff48c674b9fd10dfd911
WaloPVG <victor.barriere@outlook.fr>
2025-03-08 00:58:44 +0100

    icon XD icon

--- src/pages/pagetestwollows.jsx
+++ src/pages/pagetestwollows.jsx
@@ -23,7 +23,7 @@ function Profile() {
             })
             .catch(setError);
     }, [gameName, tagLine]);
-    
+
     return (
         <div>
             <Header />
@@ -33,9 +33,11 @@ function Profile() {
             {/* Affichage de l'icône du joueur en fonction du profileIconId */}
             {data?.summonerInfo?.profileIconId && (
                 <img
-                        src={`https://ddragon.leagueoflegends.com/cdn/15.4.1/img/profileicon/${data.summonerInfo.profileIconId}.png`}
+                        src={`https://ddragon.leagueoflegends.com/cdn/15.4.1/img/profileicon/${data.summonerInfo.profileIconId}.png?${new Date().getTime()}`}
                         alt="Icône du joueur"
                         style={{ width: 100, height: 100, borderRadius: "50%" }}
+                        onError={(e) => { e.target.src = 'fallback-image-url.png'; }} // Afficher une image de remplacement en cas d'erreur
+                        onLoad={() => console.log('Image chargée avec succès')} // Pour vérifier si l'image est bien chargée
                 />
             )}
 
